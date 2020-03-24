c9fb6da9cdb5f4a2ea23ee459089b823febc094b
Julian Villegas <julian.villegasplus@gmail.com>
2020-03-24 15:59:28 +0000

    [FIX]4-puppet_ssh_config: delete match attribute

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,25 +1,19 @@
 # ssh Client configuration file
-
 file_line { 'ssh identity':
   ensure   => present,
   path     => '/etc/ssh/ssh_config',
   line     => 'IdentityFile ~/.ssh/holberton',
-  match    => 'IdentityFile',
   multiple => false,
 }
-
 file_line { 'ssh PassAuth':
   ensure   => present,
   path     => '/etc/ssh/ssh_config',
   line     => 'PasswordAuthentication no',
-  match    => 'PasswordAuthentication',
   multiple => false,
 }
-
 file_line { 'ssh BatchMode':
   ensure   => present,
   path     => '/etc/ssh/ssh_config',
   line     => 'BatchMode no',
-  match    => 'BatchMode',
   multiple => false,
 }
